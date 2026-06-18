adb0a6df2fca6e3602d4efc58d9cd7bb4337e25c
BombyxMori1204 <alexzinovev05@gmail.com>
2026-06-18 19:23:29 +0300

    Из ниоткуда взялась галерея с авто переключением слайдов

---
+++ frontend/src/data/slides.js
@@ -0,0 +1,20 @@
+export const slides = [
+  {
+    id: 1,
+    title: 'Wuthering Waves',
+    description: 'Очень положительные (Обзоров: 49 517)',
+    image: 'https://placehold.co/1024x576?text=Slide+1',
+  },
+  {
+    id: 2,
+    title: 'Cyberpunk 2077',
+    description: 'Очень положительные (Обзоров: 612 304)',
+    image: 'https://placehold.co/1024x576?text=Slide+2',
+  },
+  {
+    id: 3,
+    title: 'Stardew Valley',
+    description: 'Чрезвычайно положительные (Обзоров: 198 442)',
+    image: 'https://placehold.co/1024x576?text=Slide+3',
+  },
+];
